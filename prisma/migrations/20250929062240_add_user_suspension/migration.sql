2fbddafd53c7ac49c2c8b20ec5c2a271df29a58f
Arsya <chmadz45@gmail.com>
2025-09-30 16:39:14 +0700

    deprected: prisma

--- prisma/migrations/20250929062240_add_user_suspension/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."User" ADD COLUMN     "suspendedUntil" TIMESTAMP(3);
--- prisma/migrations/20250929073511_fix_site_settings_cascade/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "public"."SiteSetting" DROP CONSTRAINT "SiteSetting_updatedBy_fkey";
-
--- AddForeignKey
-ALTER TABLE "public"."SiteSetting" ADD CONSTRAINT "SiteSetting_updatedBy_fkey" FOREIGN KEY ("updatedBy") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
